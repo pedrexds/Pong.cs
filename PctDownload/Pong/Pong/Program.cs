8f159d292ee10c20f382e30b496701f5c54950b3
pedrinhodias <pedropaulo-2001@hotmail.com>
2020-11-09 19:16:41 -0400

    Commit inicial

---
+++ PctDownload/Pong/Pong/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Pong
+{
+    class Program : GameWindow
+    {
+        int xDaBola = 0;
+        int yDaBola = 0;
+        int tamanhoDaBola = 20;
+        int velocidadeDaBolaEmX = 5;
+        int velocidadeDaBolaEmY = 5;
+
+        int yDoJogador1 = 0;
+        int yDoJogador2 = 0;
+
+        int xDoJogador1()
+        {
+            if(yDoJogador1 + alturaDosJogadores() / 2 > ClientSize.Height / 2)
+            {
+                yDoJogador1 = yDoJogador1 - 5;
+            }
+
+            if(yDoJogador1 - alturaDosJogadores() / 2 < -ClientSize.Height / 2)
+            {
+                yDoJogador1 = yDoJogador1 + 5;
+            }
+
+            return -ClientSize.Width / 2 + larguraDosJogadores() / 2;
+        }
+
+        int xDoJogador2()
+        {
+            if(yDoJogador2 + alturaDosJogadores() / 2 > ClientSize.Height / 2)
+            {
+                yDoJogador2 = yDoJogador2 - 5;
+            }
+
+            if (yDoJogador2 - alturaDosJogadores() / 2 < -ClientSize.Height / 2)
+            {
+                yDoJogador2 = yDoJogador2 + 5;
+            }
+
+            return ClientSize.Width / 2 - larguraDosJogadores() / 2;
+        }
+
+        int larguraDosJogadores()
+        {
+            return tamanhoDaBola;
+        }
+
+        int alturaDosJogadores()
+        {
+            return tamanhoDaBola * 3;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            xDaBola = xDaBola + velocidadeDaBolaEmX;
+            yDaBola = yDaBola + velocidadeDaBolaEmY;
+
+            if (xDaBola + tamanhoDaBola / 2 > xDoJogador2() - larguraDosJogadores() / 2
+                && yDaBola - tamanhoDaBola / 2 < yDoJogador2 + alturaDosJogadores() / 2
+                && yDaBola + tamanhoDaBola / 2 > yDoJogador2 - alturaDosJogadores() / 2)
+            {
+                velocidadeDaBolaEmX = -velocidadeDaBolaEmX;
+            }
+
+            if (xDaBola - tamanhoDaBola / 2 < xDoJogador1() + larguraDosJogadores() / 2
+                && yDaBola - tamanhoDaBola / 2 < yDoJogador1 + alturaDosJogadores() / 2
+                && yDaBola + tamanhoDaBola / 2 > yDoJogador1 - alturaDosJogadores() / 2)
+            {
+                velocidadeDaBolaEmX = -velocidadeDaBolaEmX;
+            }
+
+            if (yDaBola + tamanhoDaBola / 2 > ClientSize.Height / 2)
+            {
+                velocidadeDaBolaEmY = -velocidadeDaBolaEmY;
+            }
+
+            if (yDaBola - tamanhoDaBola / 2 < -ClientSize.Height / 2)
+            {
+                velocidadeDaBolaEmY = -velocidadeDaBolaEmY;
+            }
+
+            if(xDaBola < -ClientSize.Width / 2 || xDaBola > ClientSize.Width / 2) // Situação Gol
+            {
+                
+                xDaBola = 0;
+                yDaBola = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.W))
+            {
+                yDoJogador1 = yDoJogador1 + 5;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.S))
+            {
+                yDoJogador1 = yDoJogador1 - 5;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.Up))
+            {
+                yDoJogador2 = yDoJogador2 + 5;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.Down))
+            {
+                yDoJogador2 = yDoJogador2 - 5;
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
+
+            Matrix4 projection = Matrix4.CreateOrthographic(ClientSize.Width, ClientSize.Height, 0.0f, 1.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            DesenharRetangulo(xDaBola, yDaBola, tamanhoDaBola, tamanhoDaBola, 1.0f, 1.0f, 0.0f);
+            DesenharRetangulo(xDoJogador1(), yDoJogador1, larguraDosJogadores(), larguraDosJogadores(), 1.0f, 0.0f, 0.0f);
+            DesenharRetangulo(xDoJogador2(), yDoJogador2, larguraDosJogadores(), larguraDosJogadores(), 0.0f, 0.0f, 1.0f);
+
+            SwapBuffers();
+        }
+
+        void DesenharRetangulo(int x, int y, int largura, int altura, float r, float g, float b)
+        {
+            GL.Color3(r, g, b);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex2(-0.5f * largura + x, -0.5f * altura + y);
+            GL.Vertex2(0.5f * largura + x, -0.5f * altura + y);
+            GL.Vertex2(0.5f * largura + x, 0.5f * altura + y);
+            GL.Vertex2(-0.5f * largura + x, 0.5f * altura + y);
+            GL.End();
+        }
+
+        static void Main()
+        {
+            new Program().Run();
+
+        }
+    }
+}
